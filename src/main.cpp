2a533d5d873a07d980747e4a66deb2d9eb016b05
davidfobar <43863087+davidfobar@users.noreply.github.com>
2018-10-07 12:05:39 -0400

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -6,8 +6,6 @@
 
 #include <ApplicationMonitor.h>
 
-//Watchdog::CApplicationMonitor ApplicationMonitor(600);
-
 void setup(){
   //Serial.begin(9600);
   //Serial.println("ready");
@@ -22,17 +20,10 @@ void setup(){
   thisBrewStand.setUp();
   int loopCounter = 0;
 
-  //ApplicationMonitor.Dump(Serial);
-  //ApplicationMonitor.EnableWatchdog(Watchdog::CApplicationMonitor::Timeout_120ms);
-
   while(true){
-    //ApplicationMonitor.IAmAlive();
-    //ApplicationMonitor.SetData(0);
     loopCounter++;
     ui.update();
-    //ApplicationMonitor.SetData(1);
     thisBrewStand.update();
-    //ApplicationMonitor.SetData(2);
     ui.refresh(loopCounter);
   }
 }
